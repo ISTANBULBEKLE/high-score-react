87c899630b5a35e58e41157a230b2ada6b7bd8e8
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-10-15 13:04:44 +0100

    sorting added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import allCountryScores from "./data/allCountryScores";
 import CountryTable from "./CountryTable";
 
 function App() {
+
+  const orderedArr = allCountryScores.sort((a, b) =>
+    a.name < b.name ? -1 : a.name > b.name ? 1 : 0
+  );
+  const numericSorted = orderedArr.sort((a, b) => a.scores.s < b.scores.s ? -1 : a.scores.s > b.scores.s ? 1 : 0 );
+
   return (
     <div className="App">
       <h1 className="country-name">High Scores per Countries</h1>
-      {allCountryScores.map((country) => (
+      {numericSorted.map((country) => (
         <CountryTable
           countryName={country.name}
           countryScores={country.scores}
--- src/TableLine.jsx
+++ src/TableLine.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const TableLine = ({ name, score }) => {
   return (
     <tr className="row">
-      <td className="row-element">{name} </td>
+      <td className="row-element">{name.toUpperCase()} </td>
       <td className="row-element">{score}</td>
     </tr>
   );
